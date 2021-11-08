633ed7a06169d411ec3114fc7b0903fb13b2d2bf
xiuxiu62 <jacremer@live.com>
2021-11-08 12:32:11 -0800

    base build system

---
+++ scripts/build-linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cargo rustc -- -C link_arg=-nostartfiles
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,17 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+// static MESSAGE: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    /*    let vga_buffer = 0xb8000 as *mut u8;
 
-    HELLO.iter().enumerate().for_each(|(i, &byte)| unsafe {
-        *vga_buffer.offset(i as isize * 2) = byte;
-        *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    });
-
+        MESSAGE.iter().enumerate().for_each(|(i, &byte)| unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        });
+    */
     loop {}
 }
 
